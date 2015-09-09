3cfb1144e08f652019008502e3c95c5330aa1dfb
vnbhatia <bhatiavn@gmail.com>
2015-09-08 20:35:17 -0700

    Added post, put and get calls to the mock-server's routes

--- mock-server/routes/index.js
+++ mock-server/routes/index.js
@@ -3,10 +3,40 @@ module.exports = function(app) {
     var data = '/../../data/';
     var jsonfileservice = require('./utils/jsonfileservice')();
 
-    app.get(api + '/projects', getTodoLists);
+    app.post(api + '/projects', createProject);
+    app.put(api + '/projects/:projectName', updateProject);
+    app.get(api + '/projects/:projectName', getProject);
+    app.get(api + '/projects', getProjects);
+    app.delete(api + '/projects/:projectName', deleteProject)
 
-    function getTodoLists(req, res, next) {
-        var json = jsonfileservice.getJsonFromFile(data + 'todo-lists.json');
+    function createProject(req, res) {
+        var projectData = req.body;
+
+        todoListService.createTransaction(projectData)
+          .then(function(transaction) {
+              // Construct the full transaction before returning (with account and category)
+              return transactionService.getTransaction(transaction.id);
+          })
+          .then(function(transaction) {
+              res.send(transaction);
+          })
+          .catch(function(error) {
+              log.error(error);
+              res.status(500).send({'message': error.toString()});
+          });
+    }
+
+    function updateProject(req, res) {
+    }
+
+    function getProject(req, res) {
+    }
+
+    function getProjects(req, res) {
+        var json = jsonfileservice.getJsonFromFile(data + 'projects.json');
         res.send(json);
+    }
+
+    function deleteProject(req, res) {
     }
 };
